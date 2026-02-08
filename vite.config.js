ca53dfb08c8016fbd46bd58be6b8c11237fc9514
Basamba <papadoucoure2002@gmail.com>
2026-02-08 19:20:31 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/khadidiatouba335/portfolio.git',
+  base: '/portfolio/',
 })
